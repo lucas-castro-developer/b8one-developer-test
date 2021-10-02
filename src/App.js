0126cb046c47e2f4391994a70051a4133cf92ca6
Lucas Castro <lucas.sc98@hotmail.com>
2021-10-02 16:04:41 -0300

    Finish basic structure of product-cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import './App.css';
+import "./App.css";
+import "./assets/font-faces.css";
+import ImageTeste from "./image-teste.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product-info">
+        <div className="product-images">
+          <img src={ImageTeste} className="product-image" />
+        </div>
+        <div className="product-name">
+          Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP,
+          Gsync Série CRG50
+        </div>
+        <div className="product-price">
+          <div className="product-list-price">R$ 2.813,99</div>
+          <div className="product-selling-price">R$ 2.599,00</div>
+        </div>
+        <div className="product-installments">
+          <div>
+            em até{" "}
+            <span className="product-installments-info">10x de R$ 259,90</span>{" "}
+            sem juros
+          </div>
+        </div>
+        <button class="add-to-cart-button">Adicionar</button>
+      </div>
     </div>
   );
 }
