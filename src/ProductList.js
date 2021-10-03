1374d91e2f6596b4b82c35766eb494da0b75490d
Lucas Castro <lucas.sc98@hotmail.com>
2021-10-03 14:14:19 -0300

    Insert different values to array

--- src/ProductList.js
+++ src/ProductList.js
@@ -22,14 +22,14 @@ export const ProductList = [
     productId: 2,
     imageSrc: imageExample2,
     productName:
-      "Monitor LED 27'' Gamer Curvo Samsung 1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
+      "Monitor LED 40'' Gamer Curvo Samsung 2560 x 1280 FHD 240 Hz HDMI, DP, Gsync Série CRG55",
     productPrice: {
-      listPrice: 2813.99,
-      sellingPrice: 2599.0,
+      listPrice: 3257.99,
+      sellingPrice: 2996.85,
     },
     productInstallments: {
-      numberOfInstallments: 10,
-      installmentsPrice: 259.9,
+      numberOfInstallments: 8,
+      installmentsPrice: 374.61,
     },
     wishList: false,
     addedToCart: false,
